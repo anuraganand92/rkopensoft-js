582528b7d0f9245ff123f54095c736f0f5dfbbf1
anuraganand92 <98257282+anuraganand92@users.noreply.github.com>
2022-10-18 01:14:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function openMeal(evt, mealName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(mealName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  document.getElementById("defaultOpen").click();
